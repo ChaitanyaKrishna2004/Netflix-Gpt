751d865f277302c12c9dfa8f1a40527554aa3e50
ChaitanyaKrishna2004 <paruchurichaitanyakrishna6@gmail.com>
2025-09-17 22:11:05 +0530

    Added the google firebase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,41 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
+import { checkValidData } from '../utils/Validate';
 
 const Login = () => {
-  const [isSingForm, setIsSingForm] = useState(true);
+  const [isSignInForm, setIsSignForm] = useState(true);
+  const [errorMessage,setErrorMessage] = useState(null);  
+  const email = useRef(null);
+  const password = useRef(null);
+  const handleButtonClick = () =>{
+    // Validate the form data
+    const message = checkValidData(email.current.value,password.current.value);
+    setErrorMessage(message);
+  }
   const toggleSignInForm = () => {
-    setIsSingForm(!isSingForm);
+    setIsSignForm(!isSignInForm);
   }
   return (
     <div>
-      <Header />
+      <Header />  
       <div className='absolute'>
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/7a8c0067-a424-4e04-85f8-9e25a49a86ed/web/IN-en-20250120-TRIFECTA-perspective_860a95da-c386-446e-af83-fef8ddd80803_large.jpg"  alt="Background-Image"/>
       </div>
-      <form className='w-3/12 absolute p-12 bg-black m-36 mx-auto right-0 left-0 text-white opacity-80'>
-        <h1 className='font-bold text-3xl py-4'>{isSingForm ? "Sign In": "Sign Up"}</h1>
-        {!isSingForm && <input type='text' placeholder='Full Name' className='p-2 my-4 bg-gray-700 text-white w-full rounded-sm'/>}
-        <input type='text' placeholder='Email Address' className='p-2 my-4 bg-gray-700 text-white w-full rounded-sm'/>
-        <input type='password' placeholder='Password' className='p-2 my-4 bg-gray-700 text-white w-full rounded-sm'/>
-        <button className='p-4 my-6 bg-red-700 w-full rounded-lg'>{isSingForm ? "Sign In" : "Sing Up"}</button>
-        <p className='py-4' onClick={toggleSignInForm}>{isSingForm ? "New to Netflix?" : "Already a user"} <span className='cursor-pointer hover:underline hover:underline-offset-4'>{isSingForm ? "Sign Up Now": "Sing In Now"}.</span></p>
+      <form onSubmit={(e)=> e.preventDefault()} className='w-3/12 absolute p-12 bg-black m-36 mx-auto right-0 left-0 text-white opacity-80'>
+        <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In": "Sign Up"}</h1>
+        {!isSignInForm && <input type='text' placeholder='Full Name' className='p-2 my-4 bg-gray-700 text-white w-full rounded-sm'/>}
+       
+        <input ref={email} type='text' placeholder='Email Address' className='p-2 my-4 bg-gray-700 text-white w-full rounded-sm'/>
+       
+        <input ref={password} type='password' placeholder='Password' className='p-2 my-4 bg-gray-700 text-white w-full rounded-sm'/>
+          
+        {errorMessage != "PasswordVaild" && (
+          <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+        )}
+        
+        <button className='p-4 my-6 bg-red-700 w-full rounded-lg'onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "sign Up"}</button>
+        
+        <p className='py-4' onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix?" : "Already a user"} <span className='cursor-pointer hover:underline hover:underline-offset-4'>{isSignInForm ? "Sign Up Now": "sign In Now"}.</span></p>
       </form>
     </div>
   )
---
+++ src/utils/Firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCwa1KLtPu-VlLIURBQD31rWJktJ-Dw3ig",
+  authDomain: "netflix-gpt-55823.firebaseapp.com",
+  projectId: "netflix-gpt-55823",
+  storageBucket: "netflix-gpt-55823.firebasestorage.app",
+  messagingSenderId: "400447715937",
+  appId: "1:400447715937:web:5f28fa07b7ef66d2c3c820",
+  measurementId: "G-L0WWD6K3F7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const ANALYTICS = getAnalytics(app);
